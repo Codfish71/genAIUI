bf244c9238efad0dd16f5d5c4aba1df542304fe6
Codfish71 <prathameshpendal71@gmail.com>
2024-11-14 03:59:07 +0530

    Table Completed

--- src/ChatInterface.js
+++ src/ChatInterface.js
@@ -99,72 +99,76 @@ function ChatInterface() {
   };
 
   return (
-    <Container className="my-4 p-3 border rounded shadow-sm" style={{ maxWidth: '800px' }}>
-      {/* User Input Area with Voice Recorder Button */}
-      <Row className="mb-4">
-        <Col>
-          <InputGroup className="border rounded p-2 bg-light">
+    <Container >
+      <Container className="my-4 p-3 border rounded shadow-sm" style={{ maxWidth: '800px' }}>
+        {/* User Input Area with Voice Recorder Button */}
+        <Row className="mb-4">
+          <Col>
+            <InputGroup className="border rounded p-2 bg-light">
+              <Button
+                onClick={isListening ? handleStopListening : handleStartListening}
+                className="ml-2 rounded-circle"
+                style={{ width: '70px', height: '70px', backgroundColor: '#1e88e5', border: 'none', outline: 'none' }}
+              >
+                <span role="img" aria-label={isListening ? 'Stop Listening' : 'Start Listening'} style={{ fontSize: '1rem' }}>
+                  {isListening ? 'Stop 🔇' : 'start 🔊'}
+                </span>
+              </Button>
+            </InputGroup>
+          </Col>
+        </Row>
+        {/* Displaying User Input */}
+        <Row className="mb-4">
+          <Col>
+            <div className="p-3 border rounded text-center bg-light">
+              <p className="m-0" style={{ fontSize: '1.2rem', fontWeight: 'bold' }}>{text}</p>
+            </div>
+          </Col>
+        </Row>
+        {/* Submit Button */}
+        <Row className="mb-4">
+          <Col className="text-center">
             <Button
-              onClick={isListening ? handleStopListening : handleStartListening}
-              className="ml-2 rounded-circle"
-              style={{ width: '70px', height: '70px', backgroundColor: '#1e88e5', border: 'none', outline: 'none' }}
+              onClick={handleSubmit}
+              className="ml-2"
+              style={{ width: '100px', height: '50px', backgroundColor: '#1e88e5', border: 'none', outline: 'none' }}
             >
-              <span role="img" aria-label={isListening ? 'Stop Listening' : 'Start Listening'} style={{ fontSize: '1rem' }}>
-                {isListening ? 'Stop 🔇' : 'start 🔊'}
+              <span role="img" aria-label="Submit" style={{ fontSize: '1rem' }}>
+                Query
               </span>
             </Button>
-          </InputGroup>
-        </Col>
-      </Row>
-      {/* Displaying User Input */}
-      <Row className="mb-4">
-        <Col>
-          <div className="p-3 border rounded text-center bg-light">
-            <p className="m-0" style={{ fontSize: '1.2rem', fontWeight: 'bold' }}>{text}</p>
-          </div>
-        </Col>
-      </Row>
-      {/* Submit Button */}
-      <Row className="mb-4">
-        <Col className="text-center">
-          <Button
-            onClick={handleSubmit}
-            className="ml-2"
-            style={{ width: '100px', height: '50px', backgroundColor: '#1e88e5', border: 'none', outline: 'none' }}
-          >
-            <span role="img" aria-label="Submit" style={{ fontSize: '1rem' }}>
-              Query
-            </span>
-          </Button>
-          <Button
-            onClick={handleClear}
-            className="ml-2"
-            style={{ width: '100px', height: '50px', backgroundColor: '#1e88e5', border: 'none', outline: 'none', }}
-          >
-            <span role="img" aria-label="Submit" style={{ fontSize: '1rem' }}>
-              Clear
-            </span>
-          </Button>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <InputGroup className="border rounded p-2 bg-light">
-            <FormControl
-              as="textarea"
-              rows="4"
-              readOnly
-              value={sqlOutput || "DB Query"}
-              className="border-0 bg-light"
-              style={{ fontSize: '1rem', color: '#495057' }}
-            />
-            <Button onClick={handleDownloadOutput} className="ml-2" style={{ width: '90px', backgroundColor: '#AAF0D1', border: 'none', outline: 'none' }}>
-              <span role="img" style={{ fontSize: '3rem' }}>📂</span>
+            <Button
+              onClick={handleClear}
+              className="ml-2"
+              style={{ width: '100px', height: '50px', backgroundColor: '#1e88e5', border: 'none', outline: 'none', }}
+            >
+              <span role="img" aria-label="Submit" style={{ fontSize: '1rem' }}>
+                Clear
+              </span>
             </Button>
-          </InputGroup>
-        </Col>
-      </Row>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <InputGroup className="border rounded p-2 bg-light">
+              <FormControl
+                as="textarea"
+                rows="4"
+                readOnly
+                value={sqlOutput || "DB Query"}
+                className="border-0 bg-light"
+                style={{ fontSize: '1rem', color: '#495057' }}
+              />
+              <Button onClick={handleDownloadOutput} className="ml-2" style={{ width: '90px', backgroundColor: '#AAF0D1', border: 'none', outline: 'none' }}>
+                <span role="img" style={{ fontSize: '3rem' }}>📂</span>
+              </Button>
+            </InputGroup>
+          </Col>
+        </Row>
 
+
+      </Container>
+      {tradeData != null ? <h3>Result : </h3> : <p></p>}
       {tradeData != null ? <TradeTable data={tradeData} /> : <p></p>}
     </Container>
   );
--- src/components/TradeTable.js
+++ src/components/TradeTable.js
@@ -1,19 +1,48 @@
 import React from 'react';
 import { Table } from 'react-bootstrap';
+import "./table.css"
 
-const TradeTable = ( {data} ) => {
-    
+const TradeTable = ({ data }) => {
+    console.log(data)
+    const tableHeaders = Object.keys(data[0])
     return (
-        <Table striped bordered hover>
-            <thead>
+        <div className="mt-4 table-container">
+            <Table striped bordered hover>
+                {/* <thead>
                 <tr>
                     <th>TradeId</th>
                     <th>SourceTradeSystem</th>
                     <th>TradeManagerStatusId</th>
                     <th>InstrumentClassId</th>
-                    <th>OrderId</th>
                     <th>IsCancelled</th>
+                    <th>OrderId</th>
                     <th>IsCollateral</th>
+                    <th>IsAmend</th>
+                    <th>IsFOAmend</th>
+                    <th>IsTest</th>
+                    <th>TimeReceivedUTC</th>
+                    <th>ExecutionTimeUTC</th>
+                    <th>TradeDate</th>
+                    <th>RowVersion</th>
+                    <th>TransactionType</th>
+                    <th>Account</th>
+                    <th>Book</th>
+                    <th>BuySellInd</th>
+                    <th>Fund</th>
+                    <th>FundName</th>
+                    <th>ISIN</th>
+                    <th>GrossAmount</th>
+                    <th>Market</th>
+                    <th>Quantity</th>
+                    <th>Security</th>
+                    <th>Trader</th>
+                    <th>SourceInstrumentId</th>
+                    <th>TradeManagerDesk</th>
+                    <th>SettlementDate</th>
+                    <th>TradeCurrency</th>
+                    <th>TradeType</th>
+                    <th>PriceExecuted</th>
+                    <th>NetAmount</th>
                 </tr>
             </thead>
             <tbody>
@@ -28,8 +57,10 @@ const TradeTable = ( {data} ) => {
                         <td>{trade.IsCollateral ? 'Yes' : 'No'}</td>
                     </tr>
                 ))}
-            </tbody>
-        </Table>
+            </tbody> */}
+                <thead> <tr> {tableHeaders.map((key) => (<th key={key}>{key}</th>))} </tr> </thead> <tbody> {data.map((item, rowIndex) => (<tr key={rowIndex}> {tableHeaders.map((key) => (<td key={key}>{String(item[key])}</td>))} </tr>))} </tbody>
+            </Table>
+        </div>
     );
 };
 
